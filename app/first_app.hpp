c5f84f15703116c042695f015e4128284074a947
Nicholas <nicholascharrisofficial@outlook.com>
2023-12-15 18:46:14 -0600

    v0.3 Device Pipeline & Setup | FIX
    Fixed v0.25b to allow the project to run.
    
    Features:
    - Now detects the device and its extensions.
    
    BUG:
    Solve:
     - lve_device.cpp and lve_device.hpp had to be recreated inside of engine, no moved from one directory to another.
    
    Error:
    - Symbols not found and Compilations issues with MSVCRT. | Solved by adding /NODEFAULTLIB:MSVCRT  to command-line arguments in project properties.
    
    Version: 0.25b -> 0.3

--- app/first_app.hpp
+++ app/first_app.hpp
@@ -16,6 +16,10 @@ namespace lve{
 			// When window is created, is automatically created via its constructor, and same for the destroy.
 			LveWindow lveWindow{WIDTH, HEIGHT, "Hello Vulkan"};
 			LveDevice lveDevice{lveWindow};
-			LvePipeline lvePipeline{lveDevice, "shaders/simple_shader.vert.spv", "shaders/simple_shader.frag.spv", LvePipeline::defaultPipelineConfigInfo(WIDTH, HEIGHT)};
+			LvePipeline lvePipeline{
+				lveDevice, 
+				"shaders/simple_shader.vert.spv", 
+				"shaders/simple_shader.frag.spv", 
+				LvePipeline::defaultPipelineConfigInfo(WIDTH, HEIGHT)};
 	};
 }
